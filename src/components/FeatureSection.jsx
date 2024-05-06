92627ecbf6cffd7eb3c3265343af2453f233d83c
Shazaiib <shazaib47@hotmail.com>
2024-05-06 22:28:35 +0100

    imported features from constants and created a div to feed features through mapping from constants folder

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,3 +1,5 @@
+import { features } from "../constants";
+
 const FeatureSection = () => {
   return (
     <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
@@ -11,6 +13,21 @@ const FeatureSection = () => {
                     {" "}your code
                 </span>
             </h2>
+        </div>
+        <div className="flex flex-wrap mt-10 lg:mt-20">
+            {features.map((feature, index) => (
+                <div key={index} className="w-full sm:1/2 lg:w-1/3">
+                    <div className="flex">
+                        <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700 justify-center items-center rounded-full">
+                        {feature.icon}
+                        </div>
+                        <div>
+                            <h5 className="mt-1 mb-6 text-xl">{feature.text}</h5>
+                            <p className="te">{feature.description}</p>
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
     </div>
   );
