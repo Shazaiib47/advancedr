8e5bef2a1a77ac93cac14aa901aa701a5506e6d4
Shazaiib <shazaib47@hotmail.com>
2024-05-06 16:24:08 +0100

    created constants folder to declare variables to store the text content that will be seen on page through index.jsx

---
+++ src/constants/index.jsx
@@ -0,0 +1,20 @@
+import { BotMessageSquare } from "lucide-react";
+import { BatteryCharging } from "lucide-react";
+import { Fingerprint } from "lucide-react";
+import { ShieldHalf } from "lucide-react";
+import { PlugZap } from "lucide-react";
+import { GlobeLock } from "lucide-react";
+
+import user1 from "../assets/profile-pictures/user1.jpg"
+import user2 from "../assets/profile-pictures/user2.jpg"
+import user3 from "../assets/profile-pictures/user3.jpg"
+import user4 from "../assets/profile-pictures/user4.jpg"
+import user5 from "../assets/profile-pictures/user5.jpg"
+import user6 from "../assets/profile-pictures/user4.jpg"
+
+export const  navItems = [
+    {label: "Features", href: "#"},
+    {label: "Workflow", href: "#"},
+    {label: "Pricing", href: "#"},
+    {label: "Testimonials", href: "#"},
+]
