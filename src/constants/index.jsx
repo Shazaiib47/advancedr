fabc0b2f287846840646e757145f29709b571466
Shazaiib <shazaib47@hotmail.com>
2024-05-06 16:31:52 +0100

    added features section to be displayed

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -31,5 +31,68 @@ export const testimonials = [
         company: "Blue Horizon Technologies",
         image: user2,
         text: "I could not be happier with the outcome of the project. The team's creativity and problem solving skills were instrument in bringing our vision to life",
-    }
-]
+    },
+    {
+        user: "David Johnson",
+        company: "Quantum Innovations",
+        image: user3,
+        text: "Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable. I would highly recommend them to anyone looking for top-notch service.",
+      },
+      {
+        user: "Ronee Brown",
+        company: "Fusion Dynamics",
+        image: user4,
+        text: "Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster than we thought possible. We are grateful for their expertise and professionalism!",
+      },
+      {
+        user: "Michael Wilson",
+        company: "Visionary Creations",
+        image: user5,
+        text: "I am amazed by the level of professionalism and dedication shown by the team. They were able to exceed our expectations and deliver outstanding results.",
+      },
+      {
+        user: "Emily Davis",
+        company: "Synergy Systems",
+        image: user6,
+        text: "The team went above and beyond to ensure our project was a success. Their expertise and dedication are unmatched. I look forward to working with them again in the future.",
+      },
+];
+
+export const features = [
+    {
+      icon: <BotMessageSquare />,
+      text: "Drag-and-Drop Interface",
+      description:
+        "Easily design and arrange your VR environments with a user-friendly drag-and-drop interface.",
+    },
+    {
+      icon: <Fingerprint />,
+      text: "Multi-Platform Compatibility",
+      description:
+        "Build VR applications that run seamlessly across multiple platforms, including mobile, desktop, and VR headsets.",
+    },
+    {
+      icon: <ShieldHalf />,
+      text: "Built-in Templates",
+      description:
+        "Jumpstart your VR projects with a variety of built-in templates for different types of applications and environments.",
+    },
+    {
+      icon: <BatteryCharging />,
+      text: "Real-Time Preview",
+      description:
+        "Preview your VR application in real-time as you make changes, allowing for quick iterations and adjustments.",
+    },
+    {
+      icon: <PlugZap />,
+      text: "Collaboration Tools",
+      description:
+        "Work together with your team in real-time on VR projects, enabling seamless collaboration and idea sharing.",
+    },
+    {
+      icon: <GlobeLock />,
+      text: "Analytics Dashboard",
+      description:
+        "Gain valuable insights into user interactions and behavior within your VR applications with an integrated analytics dashboard.",
+    },
+  ];
