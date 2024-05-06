079a9b61b67ca8493a089c01cd1901c36d870967
Shazaiib <shazaib47@hotmail.com>
2024-05-06 16:58:48 +0100

    started work on navbar with tailwind css and logo import

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,18 @@
+import logo from "../assets/logo.png"
+
 const NavBar = () => {
-  return <div>NavBar</div>;
-};
+    return (
+        <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+            <div className="container px-4 mx-auto relative text-sm">
+                <div className="flex justify-content center items-center">
+                    <div className="flex items-center flex-shrink-0">
+                        <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
+                        <span className="text-xl tracking-tight">AdvancedR</span>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
 
 export default NavBar;
