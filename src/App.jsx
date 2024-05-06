c614727d471e9934f2b66253daed43aa14500056
Shazaiib <shazaib47@hotmail.com>
2024-05-06 19:22:46 +0100

    created feature component and imported onto main app whilst adding code to feature component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
 import NavBar from "./components/NavBar";
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
       <NavBar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
       <HeroSection/>
+      <FeatureSection/>
       </div>
       </>
   );
---
+++ src/components/FeatureSection.jsx
@@ -0,0 +1,11 @@
+const FeatureSection = () => {
+  return (
+    <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
+        <div className="text-center">
+
+        </div>
+    </div>
+  );
+};
+
+export default FeatureSection
