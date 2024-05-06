4499c3bb8b1c19ade9884ab98405058b5681f3aa
Shazaiib <shazaib47@hotmail.com>
2024-05-06 23:28:40 +0100

    added testimonials with mapping and styling and amended image path on index jsx

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -10,7 +10,7 @@ import user2 from "../assets/profile-pictures/user2.jpg"
 import user3 from "../assets/profile-pictures/user3.jpg"
 import user4 from "../assets/profile-pictures/user4.jpg"
 import user5 from "../assets/profile-pictures/user5.jpg"
-import user6 from "../assets/profile-pictures/user4.jpg"
+import user6 from "../assets/profile-pictures/user6.jpg"
 
 export const  navItems = [
     {label: "Features", href: "#"},
