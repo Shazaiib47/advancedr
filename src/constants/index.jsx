c54145e972b2a48a2da41631bd2b534b9194729a
Shazaiib <shazaib47@hotmail.com>
2024-05-28 18:19:22 +0100

    gave href ids in constants file and declared in components for ease of access on navbar for each section as well as adding smooth scrolling via tailwind

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -13,10 +13,10 @@ import user5 from "../assets/profile-pictures/user5.jpg"
 import user6 from "../assets/profile-pictures/user6.jpg"
 
 export const  navItems = [
-    {label: "Features", href: "#"},
-    {label: "Workflow", href: "#"},
-    {label: "Pricing", href: "#"},
-    {label: "Testimonials", href: "#"},
+    {label: "Features", href: "#features"},
+    {label: "Workflow", href: "#workflow"},
+    {label: "Pricing", href: "#pricing"},
+    {label: "Testimonials", href: "#testimonials"},
 ];
 
 export const testimonials = [
@@ -69,7 +69,7 @@ export const features = [
       icon: <Fingerprint />,
       text: "Multi-Platform Compatibility",
       description:
-        "Build VR applications that run seamlessly across multiple platforms, including mobile, desktop, and AR devices.",
+        "Build AR applications that run seamlessly across multiple platforms, including mobile, desktop, and AR devices.",
     },
     {
       icon: <ShieldHalf />,
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
   ],
   theme: {
     extend: {},
+      scrollBehavior: ['responsive'],
   },
   plugins: [],
 }
